0d649ccf7c10c202c7cb2026f116921324a58f78
Rasmus Dencker <rd@firtal.com>
2015-03-23 16:40:56 +0100

    Trait fix

--- src/Traits/HasPermissions.php
+++ src/Traits/HasPermissions.php
@@ -1,34 +1,73 @@
-<?php
+<?php namespace Dencker\Watchtower\Traits;
 
-namespace Dencker\Watchtower\Traits;
+
 use Dencker\Watchtower\Watchtower;
+use Illuminate\Session\SessionInterface;
+use Illuminate\Support\Facades\App;
 
-
-/**
- * Class HasPermissions
- * @package Dencker\Watchtower\Traits
- */
 trait HasPermissions
 {
 
     protected $watchtower = null;
 
     /**
+     * Returns this actors watchtower instance.
+     *
      * @return Watchtower
      */
-    public function watchtower()
+    public function watchtower($context = null, $session=null)
     {
-        return $this->watchtower ?: new Watchtower($this);
+
+        return $this->watchtower ?: $this->watchtower = new Watchtower($context ?: $this, $session ?: App::make('Session'));
     }
 
+
+    /**
+     * Checks whether the current actor is a super user
+     *
+     * @return bool
+     */
     public function isSuperUser()
     {
-        return !! $this->watchtower()->roles->filter(function($role){return $role->is_super_user; })->count();
+        return !! $this->watchtower()->isSuperUser();
     }
 
-    public function can($permission)
+
+    /**
+     * Alias for hasPermission()
+     *
+     * @param string $permission_code
+     *
+     * @return bool
+     *
+     */
+    public function can($permission_code)
     {
-        return $this->watchtower()->hasPermission($permission,true);
+        return $this->hasPermission($permission_code);
+    }
+
+    /**
+     * Alias for hasPermission()
+     *
+     * @param string $permission_code
+     *
+     * @return bool
+     */
+    public function hasAccess($permission_code)
+    {
+        return $this->hasPermission($permission_code);
+    }
+
+    /**
+     * Checks whether this actor has the given permission
+     *
+     * @param string $permission_code
+     *
+     * @return bool
+     */
+    public function hasPermission($permission_code)
+    {
+        return $this->watchtower()->hasPermission($permission_code);
     }
 
 }
