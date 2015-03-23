2384685881bbed088375d914bdb6d9d61cc567ad
Rasmus Dencker <rd@firtal.com>
2015-03-23 15:59:51 +0100

    Removed PHPStorm docblocks

--- src/Models/AbstractWatchtowerModel.php
+++ src/Models/AbstractWatchtowerModel.php
@@ -1,12 +1,4 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: rasmusdencker
- * Date: 18/02/15
- * Time: 22.22
- */
-
-namespace Dencker\Watchtower\Models;
+<?php namespace Dencker\Watchtower\Models;
 
 
 use Illuminate\Database\Eloquent\Model;
