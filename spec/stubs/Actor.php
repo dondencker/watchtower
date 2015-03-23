8c0cbcdaf3689f7b6fb2d4295bb3d553ac4007ce
Rasmus Dencker <rd@firtal.com>
2015-03-23 16:42:50 +0100

    Updated stub to use trait

--- spec/stubs/Actor.php
+++ spec/stubs/Actor.php
@@ -1,8 +1,10 @@
 <?php namespace spec\stubs;
 
-    use Dencker\Watchtower\Contracts\WatchtowerActorContract;
-    use Illuminate\Database\Eloquent\Model;
+use Dencker\Watchtower\Contracts\WatchtowerActorContract;
+use Dencker\Watchtower\Traits\HasPermissions;
+use Illuminate\Database\Eloquent\Model;
 
-    class Actor extends Model implements WatchtowerActorContract
-    {
-    }
+class Actor extends Model implements WatchtowerActorContract
+{
+    use HasPermissions;
+}
