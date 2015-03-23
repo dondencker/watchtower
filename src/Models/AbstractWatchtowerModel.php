dd40610888d5e4a43e4c7c575c7b7ba6827377b6
Rasmus Dencker <rd@firtal.com>
2015-03-23 16:45:27 +0100

    Cleanup + docblocks

--- src/Models/AbstractWatchtowerModel.php
+++ src/Models/AbstractWatchtowerModel.php
@@ -1,8 +1,8 @@
 <?php namespace Dencker\Watchtower\Models;
 
-
 use Illuminate\Database\Eloquent\Model;
 
-class AbstractWatchtowerModel extends Model {
-
-}
+/*
+ * Just for future use.
+ */
+class AbstractWatchtowerModel extends Model {}
